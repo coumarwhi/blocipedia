77767879aedb132e55fed2df56f7fb5db2e5adfe
coumarwhi <coumarwhi@gmail.com>
2018-10-09 12:15:56 -0700

    added wiki CRUD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery3
 //= require bootstrap
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  get "welcome/index"
 
-  get "welcome/about"
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  resources :wikis
+
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 
